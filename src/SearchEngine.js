ca13489d1b9e700242fd28531bcbc7c0eac07adc
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2018-11-29 08:53:02 -0200

    Add presentional component para o search engine

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,25 +1,7 @@
 import React, { Component } from 'react';
 
-class SearchEngine extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        origin: '',
-        destination: '',
-        departure_date: '',
-        return_date: '',
-        adults: '',
-        children: '',
-        nonstop: false,
-        travel_class: '',
-    }
-  }
-
-  render(){
-    return(
-      <div className="search-engine">
+export default props => (
+      <form className="search-engine">
         <div className="is-inline">
         <span className="checkbox">
               <input type="checkbox" name="flight-group" id="only-departure"/>
@@ -37,19 +19,27 @@ class SearchEngine extends Component {
         <div className="is-inline-input">
           <div className="search-engine-input">
             <label htmlFor="origem">Origem</label>
-            <input type="text" name="" id="origem" value={props.origin}/>
+            <input type="text" name="origin" id="origem"
+              onChange={props.handleChange}
+              value={props.origin}/>
           </div>
           <div className="search-engine-input">
             <label htmlFor="destino">Destino</label>
-            <input type="text" name="" id="destino" value={props.destination} />
+            <input type="text" name="destination" id="destino"
+              onChange={props.handleChange} 
+              value={props.destination} />
           </div>
           <div className="search-engine-input">
             <label htmlFor="ida">Ida</label>
-            <input type="text" name="" id="ida" value={props.departure_date} />
+            <input type="text" name="departure_date" id="ida" 
+              onChange={props.handleChange}
+              value={props.departure_date} />
           </div>
           <div className="search-engine-input">
             <label htmlFor="volta">Volta</label>
-            <input type="text" name="" id="volta" value={props.return_date}/>
+            <input type="text" name="return_date" id="volta" 
+              onChange={props.handleChange}
+              value={props.return_date}/>
           </div>
           <div className="search-engine-input">
             <button className="search-button" type="button" onClick={props.handleSearch}>Buscar</button>
@@ -59,7 +49,9 @@ class SearchEngine extends Component {
         <div className="is-inline-input">
           <label htmlFor="adultos" className="sr-only">Adultos</label>
           <span className="select">
-              <select name="" id="adultos" value={props.adults}>
+              <select name="adults" id="adultos" 
+                onChange={props.handleChange}
+                value={props.adults}>
                   <option value="1">1 Adulto</option>
                   <option value="2">2 Adultos</option>
                   <option value="3">3 Adultos</option>
@@ -70,7 +62,9 @@ class SearchEngine extends Component {
 
           <label htmlFor="criancas" className="sr-only">criancas</label>
           <span className="select">
-              <select name="" id="criancas" value={props.children}>
+              <select name="children" id="criancas" 
+                onChange={props.handleChange}
+                value={props.children}>
                   <option value="0">Sem crianças</option>
                   <option value="1">1 Criança</option>
                   <option value="2">2 Crianças</option>
@@ -80,9 +74,5 @@ class SearchEngine extends Component {
               </select>
           </span>
         </div>
-      </div>
+      </form>
     )
-  }
-}
-
-export default SearchEngine;
